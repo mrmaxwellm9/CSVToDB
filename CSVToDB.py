3335d048d4d63e69e09a055f4f136c72161743a1
Max Mayer <mrmaxwellm9@gmail.com>
2023-04-10 08:40:03 -0700

    Added error handling and default table/db name to the csv file name

--- CSVToDB.py
+++ CSVToDB.py
@@ -1,18 +1,30 @@
 import sqlite3
+import os
 from PyQt5.QtWidgets import *
 from PyQt5 import *
 
 # To DO
 # ------------
-#  1) Fix types (checking clumn name type right now not the data)
-#  2) Make prettier
-#  3) Add error handling
-#  4) Add dialog to confirm or reject input
-#  5) Add progress bar
 
+#  3) Add error handling
+#  5) Add progress bar
 #  7) Make csv file name default table/db name
+
 #  8) Make script to install dependencies
 #  9) Make executable
+
+#  1) Fix types (checking column name type right now not the data)
+
+# Remove failed files
+
+# Import less
+
+
+class DialogWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.widget = QWidget()
+        self.setWindowModality(QtCore.Qt.ApplicationModal)
 
 
 class MainWindow(QMainWindow):
@@ -55,13 +67,37 @@ class MainWindow(QMainWindow):
     # When the enter button is clicked
     def enter_button_clicked(self):
         if QtCore.Qt.MouseButton.LeftButton:
-            conn = sqlite3.connect((self.db_text.text()+'.db'))
+            # Open CSV file and set the database columns
+            file_name = os.path.basename(self.file_finder_text.text())
+            file_name = os.path.splitext(file_name)[0]
+            try:
+                csv_file = open(self.file_finder_text.text())
+            except:
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Critical)
+                msg.setText('Invalid CSV file')
+                msg.setWindowTitle("Error")
+                msg.exec_()
+                return
+
+            if self.table_text.text() == "":
+                self.table_text.setText(file_name)
+            if self.db_text.text() == "":
+                self.db_text.setText(file_name)
+            try:
+                conn = sqlite3.connect((self.db_text.text()+'.db'))
+            except:
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Critical)
+                msg.setText('Invalid Database Name')
+                msg.setWindowTitle("Error")
+                msg.exec_()
+                return
             cursor = conn.cursor()
 
-            # Open CSV file and set the database columns
-            csv_file = open(self.file_finder_text.text())
             header = csv_file.readline().split(",")
             header[-1] = header[-1][:-1]
+
             sql_header_line = 'CREATE TABLE ' + self.table_text.text() + '('
             for column in header:
                 column = column.replace(" ", "")
@@ -70,7 +106,7 @@ class MainWindow(QMainWindow):
                 else:
                     sql_header_line += column + " nvarchar(255), "
             sql_header_line = sql_header_line[:-2] + ")"
-
+            print(sql_header_line)
             cursor.execute(sql_header_line)
             conn.commit()
 
@@ -85,8 +121,6 @@ class MainWindow(QMainWindow):
                 for i in range(len(header)-1):
                     sql_insert_line += ", ?"
                 sql_insert_line += ")"
-                print(sql_insert_line)
-                print(content)
                 cursor.execute(sql_insert_line, content)
                 conn.commit()
             csv_file.close()
@@ -99,7 +133,11 @@ class MainWindow(QMainWindow):
 
 
 # Run the app
-connector_app = QApplication([])
-window = MainWindow()
-window.show()
-connector_app.exec()
+def runApp():
+    connector_app = QApplication([])
+    window = MainWindow()
+    window.show()
+    connector_app.exec()
+
+
+runApp()
